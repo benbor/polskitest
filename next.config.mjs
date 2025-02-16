01783caa9c475f26ac1526a8edf51e53f2b928b3
Strm <biankouski@gmail.com>
2025-02-16 16:42:47 +0100

    Updated config

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,17 @@
+
+const isProd = process.env.NODE_ENV === "production";
+
+console.log("My value");
+console.log(isProd);
+
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    output: "export",
+    basePath: isProd ? "/polskitest" : "",
+    images: {
+        unoptimized: true, // GitHub Pages doesn't support Next.js image optimization
+    },
+};
+
 
 export default nextConfig;
